88317014138035a11058d1ed03f6e6a4201fbcca
Youshou-Rhea <93993129+Youshou-Rhea@users.noreply.github.com>
2022-09-24 20:24:31 -0400

    Update base-uefi.sh
    Removed Unneeded services and groups

--- base-uefi.sh
+++ base-uefi.sh
@@ -31,15 +31,12 @@ systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 #systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
 useradd -m jzamoider
 echo jzamoider:password | chpasswd
-usermod -aG libvirt jzamoider
 
 echo "jzamoider ALL=(ALL) ALL" >> /etc/sudoers.d/jzamoider
 
