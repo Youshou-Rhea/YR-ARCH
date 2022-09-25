5454f9948ebe4ac95115c0a2c42c2803050dfe27
Youshou-Rhea <93993129+Youshou-Rhea@users.noreply.github.com>
2022-09-24 20:21:23 -0400

    Initial Commit
    Initial Commit

---
+++ base-uefi.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+hwclock --systohc
+sed -i '178s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch-laptop" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font nano
+
+#Uncomment one of the lines below depending on your graphics card. If you have intel, you can safely ignore it.
+# pacman -S --noconfirm mesa lib32-mesa #Older Intel Cards
+# pacman -S --noconfirm mesa lib32-mesa vulkan-intel #ivylake and newer
+pacman -S --noconfirm xf86-video-amdgpu mesa lib32-mesa lib32-vulkan-radeon vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m jzamoider
+echo jzamoider:password | chpasswd
+usermod -aG libvirt jzamoider
+
+echo "jzamoider ALL=(ALL) ALL" >> /etc/sudoers.d/jzamoider
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
---
+++ kde.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur/
+makepkg -si --noconfirm
+
+sudo pacman -S --noconfirm sddm systemsettings konsole bluedevil breeze-gtk kde-gtk-config kdeplasma-addons kgamma5 khotkeys kinfocenter kscreen ksshaskpass kwallet-pam kwayland-integration kwrited oxygen oxygen-sounds plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-nm plasma-pa plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-workspace-wallpapers powerdevil sddm-kcm xdg-desktop-portal-kde breeze-grub spectacle scim kaccounts-integration kio-gdrive gamescope nextcloud-client
+
+pikaur -S pamac-flatpak
+
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.brave.Browser
+
+sudo systemctl enable sddm
+sudo reboot
